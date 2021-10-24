017145b25d8a0951043940dcbd1b1c20d5d65f32
sworc <lightbox@nifty.com>
2021-10-25 08:25:11 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ public class Main extends AppWindow {
     // エントリポイント
     // *****************************************************
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 Main thisClass = new Main();
